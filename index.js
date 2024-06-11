f60fd4b131563a7407a3678a02cda55a7cecc043
sarah-abraham <sarahabraham2014@gmail.com>
2024-06-11 14:42:01 +0530

    Wrote the function to the human's choice in the game

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+let choices = ['ROCK', 'PAPER', 'SCISSORS']
 function getComputerChoice(){
-    let choices = ['Rock', 'Paper', 'Scissors']
-    selectedChoice = choices[Math.floor(Math.random()*3)]
-    return selectedChoice
+    selectedComputerChoice = choices[Math.floor(Math.random()*3)]
+    return selectedComputerChoice
 }
 
-
+function getHumanChoice(){
+    let selectedHumanChoice = prompt("ENTER ROCK, PAPER OR SCISSORS: ").toUpperCase()
+    if(!choices.includes(selectedHumanChoice)){
+        console.log("Invalid choice")
+        getHumanChoicee()
+    }
+    return selectedHumanChoice
+}
 computerChoice = getComputerChoice()
+humanChoice = getHumanChoice()
