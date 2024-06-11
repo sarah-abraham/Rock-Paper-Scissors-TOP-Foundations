fefe341b65604e325f2c96580b6d018d6d8eb778
sarah-abraham <sarahabraham2014@gmail.com>
2024-06-11 15:08:01 +0530

    Wrote function playRound to find out the winner based on choices

--- index.js
+++ index.js
@@ -1,16 +1,64 @@
 let choices = ['ROCK', 'PAPER', 'SCISSORS']
 function getComputerChoice(){
-    selectedComputerChoice = choices[Math.floor(Math.random()*3)]
+    selectedComputerChoice = choices[Math.floor(Math.random()*3)];
     return selectedComputerChoice
 }
 
 function getHumanChoice(){
-    let selectedHumanChoice = prompt("ENTER ROCK, PAPER OR SCISSORS: ").toUpperCase()
+    let selectedHumanChoice = prompt("ENTER ROCK, PAPER OR SCISSORS: ").toUpperCase();
+    if(selectedHumanChoice==='SCISSOR'){
+        selectedHumanChoice = 'SCISSORS';
+    }
     if(!choices.includes(selectedHumanChoice)){
-        console.log("Invalid choice")
-        getHumanChoicee()
+        console.log("Invalid choice");
+        getHumanChoicee();
     }
     return selectedHumanChoice
 }
-computerChoice = getComputerChoice()
-humanChoice = getHumanChoice()
+
+function playRound(humanChoice, computerChoice){
+    let flag = 0;
+    if(humanChoice == 'ROCK' && computerChoice == 'PAPER'){
+        console.log("You lose! Paper beats rock. ");
+    } 
+    else if(humanChoice == 'SCISSORS' && computerChoice == 'ROCK'){
+        console.log("You lose! Rock beats scissors.");
+    }
+    else if(humanChoice == 'PAPER' && computerChoice == 'SCISSORS'){
+        console.log("You lose! Scissors beats paper.");
+    }
+    else if(humanChoice==computerChoice){
+        console.log("It's a tie!");
+        flag = 2;
+    }
+    else{
+        console.log("You win!");
+        flag = 1;
+    }
+    return flag
+}
+
+let computerChoice = getComputerChoice();
+console.log(`Computer's choice: ${computerChoice}`)
+let humanChoice = getHumanChoice();
+let computerScore = 0;
+let humanScore = 0;
+let winner = playRound(humanChoice, computerChoice);
+if(winner==0){
+    computerScore++;
+    console.log(`Computer's score: ${computerScore}`);
+    console.log(`Your score: ${humanScore}`);
+}
+else if(winner==2){
+    computerScore++;
+    humanScore++;
+    console.log(`Computer's score: ${computerScore}`);
+    console.log(`Your score: ${humanScore}`);
+}
+else{
+    humanScore++;
+    console.log(`Computer's score: ${computerScore}`);
+    console.log(`Your score: ${humanScore}`);
+}
+
+
