c91abc33dd4b6a3efd5e83702426a73e1d7fa223
sarah-abraham <sarahabraham2014@gmail.com>
2024-06-13 08:43:27 +0530

    Replaced input textbox with buttons and accordingly updated the javascript logic to work with the new buttons

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function getComputerChoice(){
 }
 
 
-
 function playRound(humanChoice, computerChoice){
     let flag = 0;
     if(humanChoice == 'ROCK' && computerChoice == 'PAPER'){
@@ -29,41 +28,39 @@ function playRound(humanChoice, computerChoice){
 }
 
 function playGame(humanChoice){
-    let i;
-    let computerScore = 0;
-    let humanScore = 0;
-    
+        let i;
+        let computerScore = 0;
+        let humanScore = 0;
+        let computerInputContainer = document.querySelector(".computer-input");
+        computerInputContainer.removeChild(computerInputContainer.firstChild);
+        let computerChoiceDisplayElement = document.createElement("p");
+        
         let computerChoice = getComputerChoice();
         console.log(`Computer's choice: ${computerChoice}`)
-        let computerInputContainer = document.querySelector(".computer-input");
-        let computerChoiceDisplayElement = document.createElement("p");
+        
         computerChoiceDisplayElement.textContent = computerChoice;
-        computerChoiceDisplayElement.setAttribute("style",'color: black; fontSize: 20px; margin: 0px; margin-top: 5px;')
+        
         computerInputContainer.appendChild(computerChoiceDisplayElement);
-        // let humanChoice = getHumanChoice();
         let winner = playRound(humanChoice, computerChoice);
         if(winner==0){
             computerScore++;
-            // console.log(`Computer's score: ${computerScore}`);
-            // console.log(`Your score: ${humanScore}`);
         }
         else if(winner==2){
             computerScore++;
             humanScore++;
-            // console.log(`Computer's score: ${computerScore}`);
-            // console.log(`Your score: ${humanScore}`);
         }
         else{
             humanScore++;
-            // console.log(`Computer's score: ${computerScore}`);
-            // console.log(`Your score: ${humanScore}`);
-    
     }
     return [computerScore, humanScore]
 }
 
-let count = 0, computerScore = 0, humanScore = 0;
-
+const rockHumanSelection = document.querySelector(".rock-button");
+const paperHumanSelection = document.querySelector(".paper-button");
+const scissorsHumanSelection = document.querySelector(".scissors-button");
+let selectedHumanChoice = "";
+let count = 0;
+let computerScore = 0, humanScore = 0;
 const instructionsContainer = document.querySelector(".instructions");
 const roundDetails = document.createElement("p");
 roundDetails.textContent = `Round ${count + 1}`;
@@ -81,74 +78,51 @@ computerScoreCardContainer.appendChild(computerScoreElement);
 const resultContainer = document.querySelector(".result");
 const resultText = document.createElement("p");
 
-const submitButton = document.querySelector("button");
-submitButton.addEventListener('click', () => {
-
+function executeOnClick(selectedHumanChoice){
     if(count>=5){
         location.reload();
     }
     else{
-        let selectedHumanChoice = document.querySelector("input").value;
-    document.querySelector("input").value = "";
-    selectedHumanChoice = selectedHumanChoice.toUpperCase();
-    if(selectedHumanChoice==='SCISSOR'){
-        selectedHumanChoice = 'SCISSORS';
-    }
-    if(!choices.includes(selectedHumanChoice)){
-        console.log("Invalid choice");
-        // getHumanChoice();
-    }
-    let oldComputerChoice = document.querySelector(".computer-input");
-    oldComputerChoice.removeChild(oldComputerChoice.firstChild);
+        count++;
+        console.log(`Round ${count + 1}: `);
+        instructionsContainer.removeChild(roundDetails);
+        roundDetails.textContent = `Round ${count + 1}`;
+        instructionsContainer.appendChild(roundDetails);
 
 
-    console.log(selectedHumanChoice);
-    count++;
-    console.log(`Round ${count + 1}: `);
-    instructionsContainer.removeChild(roundDetails);
-    roundDetails.textContent = `Round ${count + 1}`;
-    instructionsContainer.appendChild(roundDetails);
+        let result = playGame(selectedHumanChoice);
+        console.log(result)
+        computerScore = computerScore + result[0];
+        humanScore = humanScore+result[1];
 
+        humanScoreCardContainer.removeChild(humanScoreElement);
+        computerScoreCardContainer.removeChild(computerScoreElement);
+        humanScoreElement.textContent = humanScore;
+        computerScoreElement.textContent = computerScore;
+        humanScoreCardContainer.appendChild(humanScoreElement);
+        computerScoreCardContainer.appendChild(computerScoreElement);
 
-   
-
-
-    let result = playGame(selectedHumanChoice);
-    computerScore = computerScore + result[0];
-    humanScore = humanScore+result[1];
-
-    humanScoreCardContainer.removeChild(humanScoreElement);
-    computerScoreCardContainer.removeChild(computerScoreElement);
-    humanScoreElement.textContent = humanScore;
-    computerScoreElement.textContent = computerScore;
-    humanScoreCardContainer.appendChild(humanScoreElement);
-    computerScoreCardContainer.appendChild(computerScoreElement);
-
-    if(count==5){
-        console.log(`COMPUTER'S FINAL SCORE: ${computerScore}`)
-        console.log(`YOUR FINAL SCORE: ${humanScore}`)
-        if(computerScore>humanScore){
-            resultText.textContent = "FINAL RESULT: COMPUTER WINS THE GAME! REFRESH THE PAGE TO PLAY AGAIN";
+        if(count==5){
+            console.log(`COMPUTER'S FINAL SCORE: ${computerScore}`)
+            console.log(`YOUR FINAL SCORE: ${humanScore}`)
+            if(computerScore>humanScore){
+                resultText.textContent = "FINAL RESULT: COMPUTER WINS THE GAME! REFRESH THE PAGE TO PLAY AGAIN";
+            }
+            else if(humanScore>computerScore){
+                resultText.textContent = "FINAL RESULT: YOU WON THE GAME! REFRESH THE PAGE TO PLAY AGAIN";
+            }
+            else{
+                resultText.textContent ="FINAL RESULT: IT'S A TIE! REFRESH THE PAGE TO PLAY AGAIN";
+            }
+            resultContainer.appendChild(resultText)
+            instructionsContainer.removeChild(roundDetails)
         }
-        else if(humanScore>computerScore){
-            resultText.textContent = "FINAL RESULT: YOU WON THE GAME! REFRESH THE PAGE TO PLAY AGAIN";
-        }
-        else{
-            resultText.textContent ="FINAL RESULT: IT'S A TIE! REFRESH THE PAGE TO PLAY AGAIN";
-        }
-        resultContainer.appendChild(resultText)
-        instructionsContainer.removeChild(roundDetails)
-    }
+    
     }
     
-
- ;})
-
+}
 
 
-
-
-
-
-
-
+rockHumanSelection.addEventListener('click', () => executeOnClick("ROCK"))
+paperHumanSelection.addEventListener('click', () => executeOnClick("PAPER"))
+scissorsHumanSelection.addEventListener('click', () => executeOnClick("SCISSORS"))
